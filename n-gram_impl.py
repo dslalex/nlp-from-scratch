ea51b0f395577d8b8730da96f79959793a42f2a3
Alexandre Da Silva <alexandre.da-silva@epita.fr>
2025-04-02 22:20:50 +0200

    feat(): imports

--- n-gram_impl.py
+++ n-gram_impl.py
@@ -1,4 +1,10 @@
-from tokenization.byte_pair_encoding_impl import bpe
+from datasets import load_dataset
+from collections import Counter, defaultdict
+import math
+from nltk.tokenize import word_tokenize, sent_tokenize
+
+#dataset with texts
+dataset = load_dataset('wikitext', 'wikitext-2-raw-v1')
 
 def ngram_probability():
-    # TODO
+    return
